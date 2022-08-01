cc1c32ab0048f95619248783028c7425fac8bb3e
Amy Chan <amy.chansya@gmail.com>
2022-07-31 21:17:56 -0700

    Create states to keep track of user's choices

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SingleCard from './components/SingleCard';
 
 // array of cards is constant & not going to change 
 // no need to be stored inside component state & re-evaluated every time the component is re-rendered
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png", matched: false },
+  { "src": "/img/potion-1.png", matched: false },
+  { "src": "/img/ring-1.png", matched: false },
+  { "src": "/img/scroll-1.png", matched: false },
+  { "src": "/img/shield-1.png", matched: false },
+  { "src": "/img/sword-1.png", matched: false },
 ]
 
 
@@ -23,6 +23,7 @@ function App() {
   // keep tracks of user's choices
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
 
   // shuffle cards at the beginning of a game 
@@ -36,6 +37,8 @@ function App() {
       // add 'id' property to the card object(with 'src' property in it)
       .map((card) => ({ ...card, id: Math.random() }))
 
+    setChoiceOne(null)
+    setChoiceTwo(null)
     // update card state to be the shuffled cards
     setCards(shuffledCards)
     // reset turns to 0 
@@ -44,14 +47,58 @@ function App() {
 
   // handle a choice
   const handleChoice = (card) =>{
-    console.log(card)
     // tenary operator(if ? action for true: action for false)
     // if there is a value for choiceOne, set choiceTwo to be card
     // if choiceOne is null, set choiceOne to be card
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-    console.log(choiceOne)
+
+    // state update is scheduled, not instant,
+    // cannot compare the two cards here->need useEffect hook 
   }
 
+  // compare two selected cards 
+  // function will fire when the component first mount or when a dependency changes
+  useEffect(() => {
+    // only compare when we have both choice 1 & choice 2
+    if (choiceOne && choiceTwo){
+      // disable other cards when comparing the two cards clicked
+      setDisabled(true)
+      // if two cards match
+      if(choiceOne.src === choiceTwo.src){
+        // update card state
+        setCards( prevCards => {
+          // take in prev card state, create new arr of card objs
+          return prevCards.map(card => {
+            // for the matched cards, create new obj with matched property as true
+            if (card.src === choiceOne.src){
+              return {...card, matched:true}
+            // for other cards, just return the same obj
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+      
+    }
+
+  }, [choiceOne, choiceTwo])
+  
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+  
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -63,10 +110,12 @@ function App() {
             key={card.id}
             card={card}
             handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
-
+      <p>Turns: {turns}</p>
     </div>
   );
 }
